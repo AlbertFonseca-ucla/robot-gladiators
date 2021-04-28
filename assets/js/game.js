eca30cee6dc60e022d53e270b7acf9451fea41f0
AlbertFonseca-ucla <beezyscoding@gmail.com>
2021-04-27 19:41:17 -0700

    Function and Variables

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,8 @@
-window.alert("This is an alert! JS is running!");
+var playerName = window.prompt("What is your robot's name?");
+window.alert(playerName);
+
+function fight() {
+    window.alert("The fight has begun!");
+}
+
+// fight();
