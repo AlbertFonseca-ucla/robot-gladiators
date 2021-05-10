54b7e934567f99db91a0e54677fbdb7c1a316c2a
AlbertFonseca-ucla <beezyscoding@gmail.com>
2021-05-10 00:37:32 -0700

    Skip bug fixed

--- assets/js/game.js
+++ assets/js/game.js
@@ -7,26 +7,36 @@ var randomNumber = function (min, max) {
     return value;
 }
 
+var fightOrSkip = function () {
+    // ask player if they'd like to fight or ship using fightOrSkip function
+    var promptFight = window.prompt('Would you like to FIGHT or SKIP this battle? Enter "FIGHT" or "SKIP" to choose.');
+
+    if (promptFight === "" || promptFight === null) {
+        window.alert("You need to provide a valid answer! Please try again.");
+        return fightOrSkip();
+    }
+
+    promptFight = promptFight.toLowerCase();
+    // if player picks "skip" confirm and then stop the loop
+    if (promptFight === "skip") {
+        // confirm player wants to skip
+        var confirmSkip = window.confirm("Are you sure you'd like to quit?");
+        // if yes (true), leave fight
+        if (confirmSkip) {
+            window.alert(playerInfo.name + " has decided to skip this fight. Goodbye!");
+            // subtract money from Playermoney for skipping
+            playerInfo.playerMoney = Math.max(0, playerInfo.playerMoney - 10);
+            return true;
+        }
+    }
+}
+
 // Fight Function
 var fight = function (enemy) {
 
     while (playerInfo.health > 0 && enemy.health > 0) {
-        // ask player if they'd like to fight or run
-        var promptFight = window.prompt("Would like to FIGHT or SKIP this battle? Enter 'FIGHT' or 'SKIP' to choose.");
-
-
-        if (promptFight === "skip" || promptFight === "SKIP") {
-            // confirm player wants to skip
-            var confirmSkip = window.confirm("Are you sure you'd like to quit?");
-
-            // if yes (true), leave fight
-            if (confirmSkip) {
-                window.alert(playerInfo.name + " has decided to skip this fight. Goodbye!");
-
-                playerInfo.name = Math.max(playerInfo.money - 10);
-                console.log("playerInfo.money", playerInfo.money);
-                break;
-            }
+        if (fightOrSkip()) {
+            break;
         }
         var damage = randomNumber(playerInfo.attack - 3, playerInfo.attack);
 
